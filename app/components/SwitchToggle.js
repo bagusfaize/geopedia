607b2a95e57863c22781ee355ae2377e28859ed8
bagus faize <bagusfaize@gmail.com>
2023-09-18 19:22:00 +0700

    add toggle switch button

---
+++ app/components/SwitchToggle.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const menus = [
+    {label: "Table", key: "table"},
+    {label: "Graph", key: "graph"},
+];
+
+export default function SwitchToggle({isActive, onSwitch}) {
+    return (
+        <div className="flex justify-between p-2 bg-white shadow rounded-full space-x-2">
+            {menus.map(item => {
+                return (
+                    <span 
+                        onClick={() => onSwitch(item.key)}
+                        className={`px-5 py-1 rounded-full cursor-pointer ${isActive === item.key ? "bg-blue-500 text-white" : "hover:bg-blue-50"}`}
+                    >
+                        {item.label}
+                    </span>
+                )
+            })}
+        </div>
+    )
+}
