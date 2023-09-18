97ab997766a5269076c3f1ccab281e3fc1efb12f
bagus faize <bagusfaize@gmail.com>
2023-09-18 20:05:40 +0700

    wip graph

--- app/dashboard/components/GraphView.js
+++ app/dashboard/components/GraphView.js
@@ -1,12 +1,89 @@
 import React from 'react'
+import {
+    BarChart,
+    Bar,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend
+} from "recharts";
 
-export default function GraphView() {
+const dummy = [
+    {
+        name: "Page A",
+        uv: 4000,
+        pv: 2400,
+        amt: 2400
+    },
+    {
+        name: "Page B",
+        uv: 3000,
+        pv: 1398,
+        amt: 2210
+    },
+    {
+        name: "Page C",
+        uv: 2000,
+        pv: 9800,
+        amt: 2290
+    },
+    {
+        name: "Page D",
+        uv: 2780,
+        pv: 3908,
+        amt: 2000
+    },
+    {
+        name: "Page E",
+        uv: 1890,
+        pv: 4800,
+        amt: 2181
+    },
+    {
+        name: "Page F",
+        uv: 2390,
+        pv: 3800,
+        amt: 2500
+    },
+    {
+        name: "Page G",
+        uv: 3490,
+        pv: 4300,
+        amt: 2100
+    }
+];
 
-    // if (isLoading) return <div>Loading Data...</div>
+export default function GraphView({data, isLoading}) {
+    const chartData = data?.map(item => {
+        return {
+            name: item.jenis_bahan_galian,
+            tahun: item.jumlah_produksi,
 
+        }
+    })
+    console.log('clg data', data);
     return (
         <div>
-            GraphView
+            <BarChart
+                width={1000}
+                height={300}
+                data={chartData}
+                margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5
+                }}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="tahun" fill="#8884d8" />
+                {/* <Bar dataKey="uv" fill="#82ca9d" /> */}
+            </BarChart>
         </div>
     )
 }
